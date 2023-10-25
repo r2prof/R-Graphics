0366b846f23031f561ff75f614536caa756e2875
Ali <razaalirafique@gmail.com>
2023-10-25 12:16:55 -0400

    update

---
+++ 01_req_packages.R
@@ -0,0 +1,9 @@
+install.packages("tidyverse")
+install.packages("gcookbook")
+install.packages("readxl")
+
+
+library(ggplot2)
+library(dplyr)
+library(gcookbook)
+library(readxl)
---
+++ 02_scatter_plot.R
@@ -0,0 +1,7 @@
+plot(mtcars$wt, mtcars$mpg)
+
+
+library(ggplot2)
+ggplot(mtcars, aes(x = wt, y = mpg)) +
+    geom_point()
+12
